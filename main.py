ecbcf6a4aadb5776f8edfbe16dda98459da377f2
Enis Becirebegovic <enisbe@gmail.com>
2021-01-30 09:02:03 -0700

    adding flash app

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
